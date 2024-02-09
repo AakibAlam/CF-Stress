b4d8ebbf4546f79a1daf8e62fe59db837f6cf421
AakibAlam <aakibalam20@iitk.ac.in>
2024-02-10 05:20:15 +0530

    production related changes

--- server/index.jsx
+++ server/index.jsx
@@ -25,7 +25,11 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://cfstress.azurewebsites.net",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
