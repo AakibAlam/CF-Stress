56b5547cb9415cd891e4f72d2eed76b1e2bc7d1c
AakibAlam <aakibalam20@iitk.ac.in>
2024-02-10 03:50:16 +0530

    production related changes

--- server/index.jsx
+++ server/index.jsx
@@ -33,7 +33,7 @@ app.use(
 
 app.use(express.static("../client/dist"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
 app.use(express.json());
